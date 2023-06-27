0622162ce82c510d7544f23f42c939ae2b6a94d9
Archak Nath <112867859+archakNath@users.noreply.github.com>
2023-06-27 17:10:52 +0530

    Corrected UI deficiencies

--- app/src/main/java/com/r/chuck/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/r/chuck/bmicalculator/MainActivity.kt
@@ -4,16 +4,12 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
-import kotlin.properties.Delegates
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     //declarations
